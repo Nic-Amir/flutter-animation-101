7e5580ac1e72bfe885bad98fbc406d763cb308cc
Nic Amir <amir@besquare.com.my>
2022-08-04 17:43:48 +0800

    the more the score the fast the stone

--- lib/animation_controller.dart
+++ lib/animation_controller.dart
@@ -13,18 +13,18 @@ class _SimpleScaleAnimationState extends State<SimpleScaleAnimation>
     with TickerProviderStateMixin {
   late AnimationController _animationController;
   late AnimationController _animationControllers;
-  late Animation _animation;
-  late Animation _scaleAnimation;
-  late Animation _rotateAnimation;
+
   late Animation _jumpAnimation;
   late bool _isJump = false;
   late Animation _leftrightAnimation;
+  late int score = 0;
+  int duration = 2500;
 
   @override
   void initState() {
     super.initState();
-    _animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 10));
+    _animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: duration));
 
     _animationController.addListener(() => setState(() {}));
     _animationController.repeat(reverse: true);
@@ -33,12 +33,6 @@ class _SimpleScaleAnimationState extends State<SimpleScaleAnimation>
         AnimationController(vsync: this, duration: Duration(milliseconds: 250));
     _animationControllers.addListener(() => setState(() {}));
 
-    _animation =
-        CurvedAnimation(parent: _animationController, curve: Curves.bounceOut);
-    _scaleAnimation =
-        CurvedAnimation(parent: _animationController, curve: Curves.bounceOut);
-    _rotateAnimation =
-        Tween<double>(begin: 0, end: 2 * pi).animate(_animationController);
     _jumpAnimation = Tween<Offset>(begin: Offset(0.0, 00), end: Offset(0, -400))
         .animate(CurvedAnimation(
             parent: _animationControllers, curve: Curves.decelerate));
@@ -63,47 +57,96 @@ class _SimpleScaleAnimationState extends State<SimpleScaleAnimation>
       body: Padding(
         padding: const EdgeInsets.only(bottom: 50.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Center(
-              child: Transform.translate(
-                offset: _jumpAnimation.value,
-                child: _isJump
-                    ? Image.asset(
-                        'assets/jump.png',
-                        height: 100,
-                      )
-                    : Image.asset(
-                        'assets/stay.png',
-                        height: 100,
-                      ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Text('Score: $score'),
               ),
             ),
-            Center(
-              child: Transform.translate(
-                offset: _leftrightAnimation.value,
-                child: Image.asset(
-                  'assets/stone.png',
-                  height: 100,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Center(
+                  child: Transform.translate(
+                    offset: _jumpAnimation.value,
+                    child: _isJump
+                        ? Image.asset(
+                            'assets/jump.png',
+                            height: 100,
+                          )
+                        : Image.asset(
+                            'assets/stay.png',
+                            height: 100,
+                          ),
+                  ),
                 ),
-              ),
+                Center(
+                  child: Transform.translate(
+                    offset: _leftrightAnimation.value,
+                    child: Image.asset(
+                      'assets/stone.png',
+                      height: 100,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _animationControllers.forward().whenComplete(
+                                  () => _animationControllers.reverse());
+                              _isJump = true;
+                              Future.delayed(const Duration(milliseconds: 500),
+                                  () {
+                                _isJump = false;
+                                score++;
+                                if (score > 50) {
+                                  setState(() {
+                                    _animationController.repeat(
+                                        reverse: true,
+                                        period: Duration(milliseconds: 200));
+                                  });
+                                } else if (score > 30) {
+                                  setState(() {
+                                    _animationController.repeat(
+                                        reverse: true,
+                                        period: Duration(milliseconds: 1000));
+                                  });
+                                } else if (score > 20) {
+                                  setState(() {
+                                    _animationController.repeat(
+                                        reverse: true,
+                                        period: Duration(milliseconds: 1500));
+                                  });
+                                } else if (score > 10) {
+                                  setState(() {
+                                    _animationController.repeat(
+                                        reverse: true,
+                                        period: Duration(milliseconds: 2000));
+                                  });
+                                }
+                              });
+                            });
+                          },
+                          child: Text("JUMP")),
+                      ElevatedButton(
+                          onPressed: () {
+                            score = 0;
+                            _animationController.repeat(
+                                reverse: true,
+                                period: Duration(milliseconds: 2500));
+                          },
+                          child: Text('Restart'))
+                    ],
+                  ),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _animationControllers
-                          .forward()
-                          .whenComplete(() => _animationControllers.reverse());
-                      _isJump = true;
-                      Future.delayed(const Duration(milliseconds: 500), () {
-                        _isJump = false;
-                      });
-                    });
-                  },
-                  child: Text("JUMP")),
-            )
           ],
         ),
       ),
